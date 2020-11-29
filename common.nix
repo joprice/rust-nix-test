16d9c418273a21680c3f77af6bc4154d5365443f
Joseph Price <pricejosephd@gmail.com>
2020-11-29 12:15:58 -0500

    update versions

--- common.nix
+++ common.nix
@@ -1,46 +1,27 @@
 let
-  mozOverlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  pkgs = (
-    import <nixpkgs>
-      {
-        overlays = [
-          mozOverlay
-          (
-            self: super: {
-              rustChannel = self.rustChannelOf {
-                rustToolchain = ./rust-toolchain;
-              };
-              cargo = self.rustChannel.rust;
-              rustc = self.rustChannel.rust;
-            }
-          )
-        ];
-      }
-  ).pkgsCross.armv7l-hf-multiplatform;
-  alsaLibStatic = (pkgs.alsaLib.overrideAttrs
-    (o: {
-      configureFlags = [
-        "--enable-shared=no"
-        "--enable-static=yes"
-      ];
-    }));
+  pkgs = (import
+    (builtins.fetchTarball {
+      name = "unstable";
+      url = "https://github.com/nixos/nixpkgs/archive/49715830f95f817861405c44d27312d0aa881f65.tar.gz";
+      sha256 = "0ipm0y9fnkzpq526dnih7srxm2d6gpi4zzn95r0pa5qlyqdfrzff";
+    })
+    { }).pkgsCross.armv7l-hf-multiplatform.pkgsStatic;
+  #pkgs = (import <nixpkgs> { }).pkgsCross.armv7l-hf-multiplatform.pkgsStatic;
   crateOverrides = pkgs.defaultCrateOverrides // {
     alsa-sys = attrs: attrs // {
       nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [
         pkgs.buildPackages.pkg-config
       ];
-      buildInputs = (attrs.buildInputs or [ ]) ++ [ alsaLibStatic ];
+      buildInputs = (attrs.buildInputs or [ ]) ++ [ pkgs.alsaLib ];
     };
     prost-build = attrs: attrs // {
       PROTOC = "${pkgs.buildPackages.protobuf}/bin/protoc";
     };
     client = attrs: attrs // {
       PROTOS_DIR = ./proto;
-      buildInputs = [
+      nativeBuildInputs = [
         # this is needed to format the generated grpc code
         pkgs.buildPackages.rustfmt
-        # TODO: add to all packages (expect proc macro and build scripts)?
-        # pkgs.glibc.static
       ];
     };
   };
@@ -48,10 +29,6 @@ let
     ./Cargo.nix
     {
       release = false;
-      # NOTE: cross building works with this disabled
-      targetFeatures = [
-        "crt-static"
-      ];
       defaultCrateOverrides = crateOverrides;
     };
 in
@@ -59,6 +36,6 @@ in
   pkgs = pkgs;
   build = cargo_nix.allWorkspaceMembers;
   shell = pkgs.mkShell {
-    nativeBuildInputs = [ pkgs.buildPackages.rustChannel.rust ];
+    nativeBuildInputs = [ pkgs.buildPackages.rustc ];
   };
 }
--- crate2nix.nix
+++ crate2nix.nix
@@ -1,1 +1,2 @@
-import (builtins.fetchTarball "https://github.com/lopsided98/crate2nix/tarball/d0b41938906c2fcaf86ae0b5b5a5d0d738ba1fff")
+#import (builtins.fetchTarball "https://github.com/lopsided98/crate2nix/tarball/d0b41938906c2fcaf86ae0b5b5a5d0d738ba1fff")
+import (builtins.fetchTarball "https://github.com/lopsided98/crate2nix/tarball/13edb9e57b0819b1cde45c4c2352a3ceb0f3ce9a")
